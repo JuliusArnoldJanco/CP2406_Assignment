df60f339710028cee8322995f3d58aa6b25f5b00
Julius Arnold-Janco <julius.arnoldjanco@my.jcu.edu.au>
2019-09-13 08:52:14 +1000

    Added Description to start of simulation -Main class.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
         //Basic simulation is run through road Class.
         //A new road class is being generated here.
+        System.out.println("-----------------------------CAR TRAFFIC SIMULATOR CONSOLE EDITION-----------------------------");
+        System.out.println("             -------------------CP2406: Student ID: 12891761-------------------");
+        System.out.println("No user functionality");
+        System.out.println("Car starts at speed 2 decelerates to 1 as it approaches the light");
+        System.out.println("accelerates back to 2 when light is green, it moves to new road");
+        System.out.println("When it reaches the end it returns to the start and loops again");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
         Road road = new Road();
 
     }
