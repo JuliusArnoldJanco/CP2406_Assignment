3079ccca03425305df4077eb787cd164b712a8a5
Julius Arnold-Janco <julius.arnoldjanco@my.jcu.edu.au>
2019-09-13 08:56:43 +1000

    Added comments to Vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,13 +1,18 @@
 public class Vehicle {
+    //x,y are position of vehicle on grid.
     int x;
     int y;
     int width = 0;
     int height = 0;
     int speed = 0;
 
+    //Called to update vehicles position in the simulator.
     public Vehicle(int newx, int newy, int newspeed){
+        //updates position
         x = newx;
         y = newy;
+
+        //called for setting speed, accelerating/decelerating
         speed = newspeed;
 
     }
