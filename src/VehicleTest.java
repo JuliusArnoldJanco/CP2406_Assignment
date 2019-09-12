08f59c817998a5b783a860ad649690a7c77e82e3
Julius Arnold-Janco <julius.arnoldjanco@my.jcu.edu.au>
2019-09-12 17:36:48 +1000

    Vehicle class has parameters. Vehicle Test class commit--Checking Veh. set speed condition. : Success new vehicle class is called in Main class.

--- src/VehicleTest.java
+++ src/VehicleTest.java
@@ -3,13 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
-
-    //Input speed test speed here
-
-    final int SPEED = 5;
-
+    
     @Test
     void testDefault() {
+        //Input speed test speed here
+        final int SPEED = 5;
 
         Vehicle vehicle = new Vehicle(1,1,5);
         System.out.println(vehicle.speed);
