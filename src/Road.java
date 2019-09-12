5b96614f5489b93a927b3276e935d6dc9fe98e30
Julius Arnold-Janco <julius.arnoldjanco@my.jcu.edu.au>
2019-09-12 20:08:33 +1000

    Road class is better commented.

--- src/Road.java
+++ src/Road.java
@@ -35,6 +35,8 @@ public class Road {
         Car car = new Car(ROAD_ORIGIN_X, ROAD_ORIGIN_Y + LANE_POSITION,CAR_SPEED);
         T_light t_light = new T_light();
 
+        //condition for a forever loop: 1 < 5, no step.
+
         int loop = 5;
 
         while(loop>1){
@@ -60,26 +62,38 @@ public class Road {
         }
     }
 
+    //Collision function
     private boolean Collision(int carX,int nextPos){
         boolean collision;
+        //if the space of concern doesn't contain a car return false for collision
         if (carX != nextPos){
 
             collision = false;
 
-        }else{collision = true;}
+        }
+        // If next position value = car x value, collision = true.
+        else{collision = true;}
+
+        //Return collision boolean
         return collision;
     }
 
 
+    //Change roads (road1-to-road2).
     private void ChangeRoads(Car car, T_light t_light,String anotherRoad,String t_lightState) {
+
+        // If condition for car position is 2 less than length of road (repeated for other road segment)
         if (car.getX() > (ROAD_SEGMENTS-ROAD_SEGMENTS/2) && car.getX() < ROAD_SEGMENTS || car.getX() >
                 ROAD_SEGMENTS + CROSS_SECTION_WIDTH+ROAD_SEGMENTS/2 && car.getX() <
                 (2*ROAD_SEGMENTS) + CROSS_SECTION_WIDTH && anotherRoad == "no" && t_lightState == "red") {
 
+
             //Approaches traffic light.
+            //Decelerate
             car.setSpeed(CAR_SPEED / 2);
 
 
+            //If car is in area to change traffic light to red.
             if (car.getX() > ROAD_SEGMENTS-2 && car.getX() < ROAD_SEGMENTS || car.getX() >
                     ROAD_SEGMENTS + CROSS_SECTION_WIDTH + ROAD_SEGMENTS/2 && car.getX() <
                     (2*ROAD_SEGMENTS) + CROSS_SECTION_WIDTH) {
@@ -91,21 +105,26 @@ public class Road {
                 System.out.println(t_light.getState());
             }
 
+            //If car is at end of road, change roads
             if(car.getX() > ROAD_SEGMENTS-2 && car.getX() < ROAD_SEGMENTS){
 
                 car.setX(ROAD_SEGMENTS + CROSS_SECTION_WIDTH);
 
             }
 
+            //after road is crossed
             if (car.getX() >
                     ROAD_SEGMENTS + CROSS_SECTION_WIDTH+ROAD_SEGMENTS/2 && car.getX() <
                     (2*ROAD_SEGMENTS) + CROSS_SECTION_WIDTH){
+
+                //accelerate from stop
                 car.setSpeed(CAR_SPEED/2);
             }
 
         }else{car.setSpeed(CAR_SPEED);}
 
 
+        //Condition for if reaches end of screen but not end of road
         if (car.getX() >
                 (2*ROAD_SEGMENTS-1 + CROSS_SECTION_WIDTH)  && car.getX() <
                 (2*ROAD_SEGMENTS + CROSS_SECTION_WIDTH+3) && endOfRoad == "yes"){
@@ -119,7 +138,10 @@ public class Road {
     }
 
 
+
+    // Basic counting function
     public void counter(int step,int finish) {
+
 
         counter = counter + step;
         if (counter > finish) {
@@ -128,7 +150,8 @@ public class Road {
 
         }
     }
-
+    //Basic sleep function, used in traffic light logic
+    //Will be later removed in GUI version and replaced with while loop and counter loops.
     public static void sleep(int period) {
         try {
             Thread.sleep(period);
