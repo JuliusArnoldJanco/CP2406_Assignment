be077d2b10705568ea23982389d9651b4ac3ae8f
Julius Arnold-Janco <julius.arnoldjanco@my.jcu.edu.au>
2019-11-03 22:34:34 +1100

    comments and tests have been added

--- src/LoadGameTest.java
+++ src/LoadGameTest.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 class LoadGameTest {
 
     //Testing the load game grid to make sure it matches the global grid conditions
+    //turns out it doesn't, but it shouldn't, window isnt open
     @Test
     public void testLoadGameGrid() throws FileNotFoundException {
         Assert.assertEquals(new LoadGame().getGridArray()[2],new DataPoints().getArray()[2]);
